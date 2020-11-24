b4a20303d75b3061d17f924e17c57797af352826
Josef Fürlinger <josef.fuerlinger@gmail.com>
2020-11-24 10:53:16 +0100

    enhance api call logic with authentication

--- JitBoxNg/src/app/app.component.ts
+++ JitBoxNg/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { MsalService } from "@azure/msal-angular";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 
 const API_ENDPOINT = "https://localhost:5001/weatherforecast/city/";
+const API_SCOPE = "https://jfuerlingerb2cdemo.onmicrosoft.com/api/read_data";
 
 @Component({
   selector: "app-root",
@@ -20,34 +21,42 @@ export class AppComponent {
 
   ngOnInit(): void { }
 
-  onApiCall() {
-    console.log("Api call");
-    if (this.authService.getAccount()) {
-      this.city = this.authService.getAccount().idTokenClaims.city;
+  async onApiCall() {
+    console.log("Perform api call");
+
+    if (!this.authService.getAccount()) {
+      console.log("Perform login");
+      try {
+        let result = await this.authService.loginPopup();
+        console.log("Login success", result);
+      } catch (error) {
+        console.log("Login failed : ", error);
+      }
     }
-    this.authService
-      .acquireTokenSilent({
-        scopes: ["https://jfuerlingerb2cdemo.onmicrosoft.com/api/read_data"],
-      })
-      .then((result: any) => {
-        console.log(result);
-        this.http
-          .get(API_ENDPOINT + this.city, {
-            responseType: "text",
-            headers: new HttpHeaders({
-              Authorization: "Bearer " + result.accessToken,
-            }),
-          })
-          .subscribe(
-            (result) => {
-              console.log(result);
-              var res = JSON.parse(result);
-              this.temperature = res.temperature;
-              this.humidity = res.humidity;
-              this.summary = res.summary;
-            },
-            (error) => console.log(error)
-          );
-      });
+
+    if (this.authService.getAccount()) {
+
+      this.city = this.authService.getAccount().idTokenClaims.city;
+      console.log(`city of current user: ${this.city}`);
+
+      let tokenResult = await this.authService.acquireTokenSilent({ scopes: [API_SCOPE] });
+
+      this.http
+        .get(API_ENDPOINT + this.city, {
+          responseType: "text",
+          headers: new HttpHeaders({
+            Authorization: "Bearer " + tokenResult.accessToken,
+          }),
+        })
+        .subscribe((result) => {
+          console.log(result);
+          var res = JSON.parse(result);
+          this.temperature = res.temperature;
+          this.humidity = res.humidity;
+          this.summary = res.summary;
+        },
+          (error) => console.log(error)
+        );
+    }
   }
 }
